a082e2901aba3723d8ffc51ec2c6a562d35c4102
Sajan kumar <121575502+sajan018@users.noreply.github.com>
2024-08-12 01:25:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+const { default: mongoose } = require("mongoose");
+var cors = require("cors");
+const app = express();
+
+app.use(express.json(), cors());
+const uri = process.env.MONGODB_URL;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once("open", () => console.log("Connected"));
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express app!");
+});
+
+// Product Route
+const productRoute = require("./routes/productRoute");
+app.use("/product", productRoute , cors());
+
+app.listen(5000, () => {
+  console.log(`Server running on port 5000`);
+});
