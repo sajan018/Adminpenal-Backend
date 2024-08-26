48d0a47f2284df81f6ae910ba73d172d945473c3
Sajan kumar <121575502+sajan018@users.noreply.github.com>
2024-08-26 07:12:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 const { default: mongoose } = require("mongoose");
 var cors = require("cors");
 const app = express();
+const path = require('path');
 
 app.use(express.json(), cors());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 const uri = process.env.MONGODB_URL;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -15,13 +17,14 @@ app.get("/", (req, res) => {
   res.send("Hello, this is your Express app!");
 });
 
+
 // Product Route
 const productRoute = require("./routes/productRoute");
 app.use("/product", productRoute , cors());
 
+// Admin User Route
 const adminRoute = require("./routes/userRoute");
 app.use("/admin", adminRoute , cors());
-
 
 app.listen(5000, () => {
   console.log(`Server running on port 5000`);
